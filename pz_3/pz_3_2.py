61ed33bcc65089f241d91c84f9e330c0058a2631
bogdantertychnyi <bogdantertycnyj67@gmail.com>
2024-11-09 03:11:03 +0300

    ..

--- pz_3/pz_3_2.py
+++ pz_3/pz_3_2.py
@@ -1,1 +1,17 @@
-
+num = int(input('введите номер дня недели '))
+if num == 1:
+  print("понедельник")
+elif num == 2:
+  print("вторник")
+elif num == 3:
+  print("среда")
+elif num == 4:
+  print("четверг")
+elif num == 5:
+  print("пятница")
+elif num == 6:
+  print("суббота")
+elif num == 7:
+  print("воскресенье")
+else:
+ print("введите число от 1 до 7 ")
