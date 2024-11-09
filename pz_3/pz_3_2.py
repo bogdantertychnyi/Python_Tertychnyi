f91a6970a509601d2e04d87ed73b0252098d7c36
bogdantertychnyi <bogdantertycnyj67@gmail.com>
2024-11-09 03:15:14 +0300

    конец

--- pz_3/pz_3_2.py
+++ pz_3/pz_3_2.py
@@ -1,17 +1,24 @@
-num = int(input('введите номер дня недели '))
-if num == 1:
-  print("понедельник")
-elif num == 2:
-  print("вторник")
-elif num == 3:
-  print("среда")
-elif num == 4:
-  print("четверг")
-elif num == 5:
-  print("пятница")
-elif num == 6:
-  print("суббота")
-elif num == 7:
-  print("воскресенье")
-else:
- print("введите число от 1 до 7 ")
+num = input('введите номер дня недели ')
+while type(num) != int:
+  try:
+    num = int(num)
+    if num == 1:
+      print("понедельник")
+    elif num == 2:
+      print("вторник")
+    elif num == 3:
+      print("среда")
+    elif num == 4:
+      print("четверг")
+    elif num == 5:
+      print("пятница")
+    elif num == 6:
+      print("суббота")
+    elif num == 7:
+      print("воскресенье")
+    else:
+      print("введите число от 1 до 7 ")
+      num = input('введите номер дня недели ')
+  except ValueError:
+    print("Неправильно ввели!")
+    num = input('введите номер дня недели ')
