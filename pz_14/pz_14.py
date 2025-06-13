ecee5ed941464d7cbed0e42980214e519c090ac2
bogdantertychnyi <bogdantertycnyj67@gmail.com>
2025-06-13 14:05:00 +0300

    исправление111

--- pz_14/pz_14.py
+++ pz_14/pz_14.py
@@ -4,15 +4,17 @@
 
 import re
 
-pattern_point = re.compile(r'\b(d{2}).(d{2}).(d{4})\b')
-pattern_slash = re.compile(r'\b(d{2})/(d{2})/(d{4})\b')
+# В исходном текстовом файле(dates.txt) найти все даты в форматах ДД.ММ.ГГГГ и
+# ДД/ММ/ГГГГ. Посчитать количество дат в каждом формате. Поместить в новый
+# текстовый файл все даты февраля в формате ДД/ММ/ГГГГ
+
+import re
 
 file = open('dates.txt', 'r', encoding='utf-8')
 content = file.read()
-file.close()
 
-point = pattern_point.findall(content)
-slash = pattern_slash.findall(content)
+point = re.findall(r'\b(\d{2})\.(\d{2})\.(\d{4})\b', content)
+slash = re.findall(r'\b(\d{2})/(\d{2})/(\d{4})\b', content)
 
 print(f'ДД.ММ.ГГГГ: {len(point)}')
 print(f'ДД/ММ/ГГГГ: {len(slash)}')
@@ -20,5 +22,5 @@ print(f'ДД/ММ/ГГГГ: {len(slash)}')
 slash_february = [f"{d[0]}/{d[1]}/{d[2]}" for d in slash if d[1] == '02']
 
 filee = open('slash_february.txt', 'w', encoding='utf-8')
-filee.write('\n'.join(slash_february))
+filee.write('\n'.join(map(str, slash_february)))
 filee.close()
